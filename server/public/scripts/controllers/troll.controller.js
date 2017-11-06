e089ba65917d7b111916e156f193c53ff102b048
Josh Nothum <joshnothum@gmail.com>
2017-11-06 15:05:13 -0600

    can add trolls; delete button is not functional

--- server/public/scripts/controllers/troll.controller.js
+++ server/public/scripts/controllers/troll.controller.js
@@ -1,18 +1,41 @@
 myApp.controller('TrollController', function ($http) {
-    
+
     var troll = this;
-    troll.newTroll = {
-        name:'',
-        message:''
+    newTroll = {};
+    allTrolls = {};
+
+
+    troll.refreshTroll = function () {
+        $http.get('/troll').then(function (success) {
+            troll.allTrolls = success.data;
+        }).catch(function (error) {
+            console.log('error', error);
+
+        });
+
+    };
+    troll.addTroll = function (newTroll) {
+        $http.post('/troll', newTroll).then(function (success) {
+            console.log(newTroll);
+            troll.refreshTroll();
+        }).catch(function (error) {
+            console.log('We might have a problem:', error);
+
+        });
+    };
+    troll.deleteTroll = function (trollId) {
+        console.log('troll id:',trollId);
+
+        $http.delete('/troll/' + trollId).then(function (response) {
+
+            console.log(reponse);
+
+        }).catch(function (error) {
+            console.log('deleteTrolls error:', error);
+
+        });
+        troll.refreshTroll();
     };
 
-    troll.addTroll = function (newTroll) {
-    $http.post('/troll', newTroll).then(function (success) {
-        console.log(newTroll);
-        
-    }).catch(function (error) {
-        console.log('We might have a problem:', error);
-        
-    });      
-    };
+    troll.refreshTroll();
 });
--- server/routers/troll.router.js
+++ server/routers/troll.router.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
-// Make sure to Google mongoose when looking for resources
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TrollSchema = new Schema({ name: String, message: String });
-// Mongoose gives us an object that has properties we
-// can use to talk to the database.
+
 var Troll = mongoose.model('troll', TrollSchema, 'trolls');
 
 router.post('/', function (req, res) {
@@ -17,9 +15,41 @@ router.post('/', function (req, res) {
         if(err){
         console.log('trolls are not having a great time in here:', err);
         }else{
-            res.sendStatus(500);
+            res.sendStatus(200);
         }
     });//end of trollToAdd
     
 });//end of post
+
+router.get('/', function (req, res) {
+    Troll.find({}, function (err, foundTrolls) {
+        if (err) {
+            console.log('Oh, No!:', err);
+            res.sentStatus(501);
+        } else {
+            console.log(foundTrolls);
+            res.send(foundTrolls);
+
+        }//end of if/else
+    });//
+
+});//end of router.get
+
+router.delete('/:id', function (req, res) {
+    console.log(req.params);
+    var TrollId = req.params.id;
+
+    Troll.findByIdAndRemove({ "_id": TrollId }, function (err, data) {
+
+        if (err) {
+            console.log('Oh, No! We got issues in the delete route:', err);
+            res.sentStatus(501);
+        } else {
+            res.send(200);
+
+        }//end of if/else
+    });//end of findByIdAndRemove
+
+});//end of router.delete
+
 module.exports = router;
